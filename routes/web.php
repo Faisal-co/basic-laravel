ef400fdc84215a21bef1ad0de4762dc9d97aff4f
Faisal <softtechrana05@gmail.com>
2026-02-10 00:23:02 +0500

    add-indexblade

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/hello', function () {
-    return view('hello');
-});
+/* Route::get('/home', function () {                // OR Use below way to route.
+    return view('home'); 
+}); */
+
+Route::view('/', 'home');
+Route::get('/products',[ProductController::class,'abcindex']);
 
